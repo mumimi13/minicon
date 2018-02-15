6b40837dd4db9f6d230f58bf9143a84e6d3aa3fa
Misaki Uchiyama <ec2-user@ip-172-31-33-234.us-east-2.compute.internal>
2018-02-15 10:31:36 +0000

    create event's show page

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,6 +1,11 @@
 class Admin::EventsController < ApplicationController
   layout 'admin'
+
   def index
     @events = Event.all
+  end
+  
+  def show
+    @event = Event.find(params[:id])
   end
 end
