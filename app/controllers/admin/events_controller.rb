c49bbfd659befaea99cf2c035a5f759533897b34
Misaki Uchiyama <ec2-user@ip-172-31-33-234.us-east-2.compute.internal>
2018-02-15 11:04:12 +0000

    create event create

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -12,4 +12,12 @@ class Admin::EventsController < ApplicationController
   def new
     @event = Event.new
   end
+  
+  def create
+    event_params = params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
+    @event = Event.new(event_params)
+    @event.save
+    flash[:notice] = 'Event was successfully created.'
+    redirect_to admin_events_path(@event)
+  end
 end
